eaae805a17c8f35dd213382f5dcf75ee9f7a5cf6
Patrick Nicholas <patricknicholas@foxmail.com>
2018-04-25 11:52:46 +0800

    [helper] add tool methods

--- file/helper.go
+++ file/helper.go
@@ -50,3 +50,36 @@ func IsExists(name string) bool {
 	}
 	return true
 }
+
+// CreateWhenNotExists create dir with 0766 when it not exists.
+func CreateWhenNotExists(dir string) error {
+	if !IsExists(dir) {
+		if err := os.MkdirAll(dir, 0766); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ReadChildrenDir returns child directories of parent in sorted order.
+func ReadChildrenDir(parent string) ([]string, error) {
+	dir, err := os.Open(parent)
+	if err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+
+	fileInfo, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	var names []string
+	for _, info := range fileInfo {
+		if info.IsDir() {
+			names = append(names, info.Name())
+		}
+	}
+	sort.Strings(names)
+	return names, nil
+}
